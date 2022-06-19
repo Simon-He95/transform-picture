27ccf04d1ca3d2f2284a05878f13b8d8adfea4c3
Simon he <13917107460@163.com>
2022-06-20 00:45:09 +0800

    chore: style

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,40 +1,37 @@
 <script setup lang="ts">
-import { toPDF, transformPicture } from '../../src'
-let baseURL = $ref('')
-let output = $ref('')
-let filename = $ref('')
-let src = $ref('')
+import { toPDF, transformPicture } from "../../src";
+let baseURL = $ref("");
+let output = $ref("");
+let filename = $ref("");
+let src = $ref("");
 function submitPic(e) {
-  const reads = new FileReader()
-  const f = document.getElementById('file').files[0]
-  filename = f.name.slice(0, f.name.lastIndexOf('.'))
-  reads.readAsDataURL(f)
+  const reads = new FileReader();
+  const f = document.getElementById("file").files[0];
+  filename = f.name.slice(0, f.name.lastIndexOf("."));
+  reads.readAsDataURL(f);
   reads.onload = (e) => {
-    src = baseURL = e.target.result
-    transformPic()
-  }
+    src = baseURL = e.target.result;
+    transformPic();
+  };
 }
 
 async function transformPic() {
-  if (!baseURL)
-    return
-  output = await transformPicture(baseURL)
-  document.getElementById('outImg').src = output.toDataURL()
+  if (!baseURL) return;
+  output = await transformPicture(baseURL);
+  document.getElementById("outImg").src = output.toDataURL();
 }
 
 function download(type) {
-  if (!output)
-    return
-  if (type === 'pdf')
-    return toPDF(output, filename)
-  const a = document.createElement('a')
-  a.href = output.toDataURL(`image/${type}`)
-  a.download = `${filename}.${type}`
-  a.click()
+  if (!output) return;
+  if (type === "pdf") return toPDF(output, filename);
+  const a = document.createElement("a");
+  a.href = output.toDataURL(`image/${type}`);
+  a.download = `${filename}.${type}`;
+  a.click();
 }
 
 function upload() {
-  file.click()
+  file.click();
 }
 </script>
 
@@ -48,7 +45,7 @@ function upload() {
       hidden
       accept="image/*"
       @change="submitPic"
-    >
+    />
     <button
       btn
       flex="~ gap-1"
@@ -72,7 +69,8 @@ function upload() {
         <path
           fill="currentColor"
           d="M1280 1408q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm256 0q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm128-224v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h427q21 56 70.5 92t110.5 36h256q61 0 110.5-36t70.5-92h427q40 0 68 28t28 68zm-325-648q-17 40-59 40h-256v448q0 26-19 45t-45 19H704q-26 0-45-19t-19-45V576H384q-42 0-59-40q-17-39 14-69L787 19q18-19 45-19t45 19l448 448q31 30 14 69z"
-        /></svg>上传图片
+        /></svg
+      >上传图片
     </button>
     <div flex="~ gap-1" justify-center items-center>
       <span text-xs font-bold>图片名：</span>
@@ -84,7 +82,7 @@ function upload() {
         border-1
         border-yellow
         border-rd-1
-      >
+      />
     </div>
     <vivid-typing
       content="支持修改图片名"
@@ -94,15 +92,15 @@ function upload() {
       text-sm
       :interval="200"
     />
-    <div w-50 min-h-30 ma relative bg-gray-300:30 border-rd-1 m-y-5>
-      <img v-if="src" id="img" :src="src" w-full alt="上传图片" ma>
+    <div w-50 min-h-30 ma relative :class="!src && 'bg-gray-300:30'" border-rd-1 m-y-5>
+      <img v-if="src" id="img" :src="src" w-full alt="上传图片" ma />
       <img
         v-else
         src="/photo.png"
         alt="empty"
         absolute
         style="left: 50%; top: 50%; transform: translate(-50%, -50%)"
-      >
+      />
     </div>
     <div inline-flex gap-2 m-y-5>
       <button
@@ -127,7 +125,8 @@ function upload() {
           <path
             fill="currentColor"
             d="M1280 1344q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm256 0q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm128-224v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h465l135 136q58 56 136 56t136-56l136-136h464q40 0 68 28t28 68zm-325-569q17 41-14 70l-448 448q-18 19-45 19t-45-19L339 621q-31-29-14-70q17-39 59-39h256V64q0-26 19-45t45-19h256q26 0 45 19t19 45v448h256q42 0 59 39z"
-          /></svg>PNG
+          /></svg
+        >PNG
       </button>
 
       <button
@@ -152,7 +151,8 @@ function upload() {
           <path
             fill="currentColor"
             d="M1280 1344q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm256 0q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm128-224v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h465l135 136q58 56 136 56t136-56l136-136h464q40 0 68 28t28 68zm-325-569q17 41-14 70l-448 448q-18 19-45 19t-45-19L339 621q-31-29-14-70q17-39 59-39h256V64q0-26 19-45t45-19h256q26 0 45 19t19 45v448h256q42 0 59 39z"
-          /></svg>JPG
+          /></svg
+        >JPG
       </button>
       <button
         btn
@@ -176,7 +176,8 @@ function upload() {
           <path
             fill="currentColor"
             d="M1280 1344q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm256 0q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm128-224v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h465l135 136q58 56 136 56t136-56l136-136h464q40 0 68 28t28 68zm-325-569q17 41-14 70l-448 448q-18 19-45 19t-45-19L339 621q-31-29-14-70q17-39 59-39h256V64q0-26 19-45t45-19h256q26 0 45 19t19 45v448h256q42 0 59 39z"
-          /></svg>JPEG
+          /></svg
+        >JPEG
       </button>
       <button
         btn
@@ -200,11 +201,12 @@ function upload() {
           <path
             fill="currentColor"
             d="M1280 1344q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm256 0q0-26-19-45t-45-19t-45 19t-19 45t19 45t45 19t45-19t19-45zm128-224v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h465l135 136q58 56 136 56t136-56l136-136h464q40 0 68 28t28 68zm-325-569q17 41-14 70l-448 448q-18 19-45 19t-45-19L339 621q-31-29-14-70q17-39 59-39h256V64q0-26 19-45t45-19h256q26 0 45 19t19 45v448h256q42 0 59 39z"
-          /></svg>PDF
+          /></svg
+        >PDF
       </button>
     </div>
 
-    <img id="outImg" width="200" ma>
+    <img id="outImg" width="200" ma />
     <Footer />
   </main>
 </template>
